71fd21ba84ec6729854689af04da9d13905a29f9
Denis Tamoykin <denistamoykin04@gmail.com>
2026-05-26 21:00:52 +0300

    добавить согласие на пд

--- tg_bot/keyboards/main_menu.py
+++ tg_bot/keyboards/main_menu.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from aiogram.types import KeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
 
 from config import ButtonConfig
-from tg_bot.keyboards.base import BaseReplyKeyboard
+from tg_bot.keyboards.base import BaseInlineKeyboard, BaseReplyKeyboard
 
 
 class MainMenuKeyboard(BaseReplyKeyboard):
@@ -22,3 +22,24 @@ class MainMenuKeyboard(BaseReplyKeyboard):
         ]
 
         return self._build_reply_markup(buttons)
+
+
+class PDAgreementKeyboard(BaseInlineKeyboard):
+    """Инлайн-клавиатура для согласия на обработку ПДн."""
+
+    def get_markup(self) -> InlineKeyboardMarkup:
+        buttons = [
+            [InlineKeyboardButton(text=ButtonConfig.PD_AGREE_BUTTON, callback_data='pd_agree')],
+            [InlineKeyboardButton(text=ButtonConfig.PD_DISAGREE_BUTTON, callback_data='pd_disagree')],
+        ]
+        return self._build_inline_markup(buttons)
+
+
+class PDRetryKeyboard(BaseInlineKeyboard):
+    """Инлайн-клавиатура для повторного запроса согласия."""
+
+    def get_markup(self) -> InlineKeyboardMarkup:
+        buttons = [
+            [InlineKeyboardButton(text=ButtonConfig.PD_RETRY_BUTTON, callback_data='pd_retry')]
+        ]
+        return self._build_inline_markup(buttons)
