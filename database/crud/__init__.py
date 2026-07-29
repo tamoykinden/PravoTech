8da233de6aa89bd9b1c7ef4c627e6a95a42329b4
Denis Tamoykin <denistamoykin04@gmail.com>
2026-07-29 10:01:26 +0300

    style(database): format CRUD exports

--- database/crud/__init__.py
+++ database/crud/__init__.py
@@ -2,4 +2,9 @@ from database.crud.case import CaseCRUD
 from database.crud.category import CategoryCRUD
 from database.crud.document import DocumentCRUD
 from database.crud.feedback import TGFeedbackCRUD, VKFeedbackCRUD
-from database.crud.user import TGUserCRUD, TGUserRequestCRUD, VKUserCRUD, VKUserRequestCRUD
+from database.crud.user import (
+    TGUserCRUD,
+    TGUserRequestCRUD,
+    VKUserCRUD,
+    VKUserRequestCRUD,
+)
